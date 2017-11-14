875a3de9307b0ae3b2bd351dee04725653f002a3
nicozar95 <nicolas.zarewsky@wolox.com.ar>
2017-11-14 10:12:23 -0300

    Fixed book test

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,17 +3,17 @@ require 'faker'
 require 'rspec-rails'
 
 describe Book, type: :model do
-  it should validate_presence_of(:genre)
+  it { should validate_presence_of(:genre) }
 
-  it should validate_presence_of(:author)
+  it { should validate_presence_of(:author) }
 
-  it should validate_presence_of(:title)
+  it { should validate_presence_of(:title) }
 
-  it should validate_presence_of(:image)
+  it { should validate_presence_of(:image) }
 
-  it should validate_presence_of(:publisher)
+  it { should validate_presence_of(:publisher) }
 
-  it should validate_presence_of(:year)
+  it { should validate_presence_of(:year) }
 
   subject(:book) do
     Book.new(
@@ -33,43 +33,43 @@ describe Book, type: :model do
   let(:year) { Faker::Number.between(1000, 2018).to_s }
   let(:image) { 'test' }
 
-  it is_expected.to be_valid
+  it { is_expected.to be_valid }
 
   describe '#create' do
     context 'When the author is nil' do
       let(:author) { nil }
 
-      it is_expected.to be_invalid
+      it { is_expected.to be_invalid }
     end
 
     context 'When the genre is nil' do
       let(:genre) { nil }
 
-      it is_expected.to be_invalid
+      it { is_expected.to be_invalid }
     end
 
     context 'When the title is nil' do
       let(:title) { nil }
 
-      it is_expected.to be_invalid
+      it { is_expected.to be_invalid }
     end
 
     context 'When the publisher is nil' do
       let(:publisher) { nil }
 
-      it is_expected.to be_invalid
+      it { is_expected.to be_invalid }
     end
 
     context 'When the year is nil' do
       let(:year) { nil }
 
-      it is_expected.to be_invalid
+      it { is_expected.to be_invalid }
     end
 
     context 'When the image is nil' do
       let(:image) { nil }
 
-      it is_expected.to be_invalid
+      it { is_expected.to be_invalid }
     end
   end
 end
