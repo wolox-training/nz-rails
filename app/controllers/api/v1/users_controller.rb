8b16de75b34adc42151a6da5f92bd21bfd979db0
nicozar95 <nicolas.zarewsky@wolox.com.ar>
2017-11-14 09:53:35 -0300

    Added hash syntax

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,9 +4,9 @@ module Api
       def create
         @user = User.new(user_params)
         if @user.save
-          render :json => @user
+          render json: @user
         else
-          render :json => { :errors => @user.errors.full_messages }
+          render json: { errors: @user.errors.full_messages }
         end
       end
 
