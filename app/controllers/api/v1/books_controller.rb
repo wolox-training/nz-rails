e8407ffeda9f66923f23eba381e6e90b6115edfa
nicozar95 <nicolas.zarewsky@wolox.com.ar>
2017-11-15 11:21:21 -0300

    Added filter in index page

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -3,12 +3,21 @@ module Api
     class BooksController < ApplicationController
       include Wor::Paginate
       def index
-        render_paginated Book
+        render_paginated Book.where(book_params)
       end
 
       def show
         @book = Book.find(params[:id])
         render json: @book
+      end
+
+      private def book_params
+        params.permit(:author,
+          :title,
+          :genre,
+          :year,
+          :image,
+          :author)        
       end
     end
   end
