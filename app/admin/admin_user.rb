54a6817d4c2ef08a54e57d27273897b6afcf04a7
nicozar95 <nicolas.zarewsky@wolox.com.ar>
2017-11-15 10:34:34 -0300

    Fixed admin user creation

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -5,8 +5,6 @@ ActiveAdmin.register AdminUser do
     selectable_column
     id_column
     column :email
-    column :first_name
-    column :last_name
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
@@ -17,16 +15,12 @@ ActiveAdmin.register AdminUser do
   filter :current_sign_in_at
   filter :sign_in_count
   filter :created_at
-  filter :first_name
-  filter :last_name
 
   form do |f|
     f.inputs do
       f.input :email
       f.input :password
       f.input :password_confirmation
-      f.input :first_name
-      f.input :last_name
     end
     f.actions
   end
