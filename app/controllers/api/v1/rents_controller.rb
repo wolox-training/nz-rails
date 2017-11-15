49d1d476ce6fdd9953669e78a2e8f95f909268f8
nicozar95 <nicolas.zarewsky@wolox.com.ar>
2017-11-15 12:08:08 -0300

    Added index view for users rents

---
+++ app/controllers/api/v1/rents_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  module V1
+    class RentsController < ApiController
+      include Wor::Paginate
+      skip_before_action :authenticate_request, only: [:index]
+
+      def index
+        render_paginated Rent.where(user: params[:user_id])
+      end
+
+      def create
+        @rent = Rent.new(rent_params)
+        if @rent.save
+          render json: @rent
+        else
+          render json: { errors: @rent.errors.full_messages },status: 400
+        end
+      end
+
+      private def rent_params
+        params.permit(:user,:book,:from,:to)
+      end
+    end
+  end
+end
