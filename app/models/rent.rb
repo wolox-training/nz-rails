08c3379507499f561d1dc1a29c660de179f94fb2
nicozar95 <nicolas.zarewsky@wolox.com.ar>
2017-11-21 09:00:11 -0300

    Remove multiple migration with the same name

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,4 +1,5 @@
 class Rent < ApplicationRecord
-  belongs_to :user, :book, required: true
+  belongs_to :user, required: true
+  belongs_to :book, required: true
   validates :user, :book, :from, :to, presence: true
 end
--- db/migrate/20171110213312_add_verification_code_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddVerificationCodeToUsers < ActiveRecord::Migration[5.1]
-    def change
-        add_column :users, :verification_code, :string, null: false
-    end
-end
