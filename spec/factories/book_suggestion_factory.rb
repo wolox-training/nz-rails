ebdb36933ccb81463b4c850addf174359cf1428c
nicozar95 <nicolas.zarewsky@wolox.com.ar>
2017-11-22 15:41:10 -0300

    Added book-suggestion test

--- spec/factories/book_suggestion_factory.rb
+++ spec/factories/book_suggestion_factory.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :book_suggestion do
-    editorial {Faker::Fillmurray.image}
-    price  {Faker::Commerce.price}
-    author {Faker::Book.author}
-    title {Faker::Book.title}
-    link {Faker::Fillmurray.image}
-    publisher {Faker::Book.publisher}
-    year {Faker::Number.between(1500, 3000).to_s}
+    editorial { Faker::Fillmurray.image }
+    price  { Faker::Commerce.price }
+    author { Faker::Book.author }
+    title { Faker::Book.title }
+    link { Faker::Fillmurray.image }
+    publisher { Faker::Book.publisher }
+    year { Faker::Number.between(1500, 3000).to_s }
     user_id nil
   end
 end
