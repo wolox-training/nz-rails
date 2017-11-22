8eed7df8c5f8705225e850539c17d36a8fa512f4
nicozar95 <nicolas.zarewsky@wolox.com.ar>
2017-11-22 14:31:20 -0300

    Fix in bundle exec rubocop app spec -R --format simple

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -6,7 +6,6 @@ module Api
       protect_from_forgery
       before_action :authenticate_request, except: [:create]
 
-
       ENTITY_KEY = :email
 
       def entity_payload(entity)
@@ -14,13 +13,10 @@ module Api
       end
 
       def find_authenticable_entity(entity_payload_returned_object)
-       @current_user ||= User.find_by(email: entity_payload_returned_object.fetch(ENTITY_KEY))
+        @current_user ||= User.find_by(email: entity_payload_returned_object.fetch(ENTITY_KEY))
       end
 
-      def current_user
-        @current_user
-      end
-
+      attr_reader :current_user
     end
   end
 end
--- app/controllers/api/v1/book_suggestions_controller.rb
+++ app/controllers/api/v1/book_suggestions_controller.rb
@@ -6,16 +6,23 @@ module Api
       def create
         @book_suggestion = BookSuggestion.new(book_suggestion_param
           .merge(user_id: current_entity.id))
-          if @book_suggestion.save
-            render json: @book_suggestion, status: :ok
-          else
-            render json: { errors: @book_suggestion.errors.full_messages }, status: 201
-          end
+        if @book_suggestion.save
+          render json: @book_suggestion, status: :ok
+        else
+          render json: { errors: @book_suggestion.errors.full_messages }, status: 201
+        end
       end
 
       private def book_suggestion_param
-        params.require(:book_suggestion).permit(:author,:title,:link,:year,:publisher,:editorial,:price)
-
+        params.require(:book_suggestion).permit(
+          :author,
+          :title,
+          :link,
+          :year,
+          :publisher,
+          :editorial,
+          :price
+        )
       end
     end
   end
