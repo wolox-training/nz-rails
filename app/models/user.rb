b707dabc73cc4c8ae246d1f1bf318de0cbf7dd40
nicozar95 <nicolas.zarewsky@wolox.com.ar>
2017-11-24 16:36:05 -0300

    Added google OAuth authentication to create a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -11,5 +12,10 @@ class User < ApplicationRecord
 
   def name
     email
+  end
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+    user
   end
 end
