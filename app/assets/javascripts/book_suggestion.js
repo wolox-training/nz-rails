0ee345556f02b8e575d95351e6f286903a36c10d
nicozar95 <nicolas.zarewsky@wolox.com.ar>
2017-11-24 11:24:12 -0300

    Added functional form with jQuerry

--- app/assets/javascripts/book_suggestion.js
+++ app/assets/javascripts/book_suggestion.js
@@ -1,24 +1,11 @@
 $(document).ready(function(){
-  $("input").click(function(){
-    var id =$('form').attr('id');
-    debugger
-    $.ajax({
-      url : '/book_suggestions/' + id,
-      type : 'GET',
-      dataType : 'json',
-      success : function(book_suggestion) {
-          $('table').append("<tr><td>" + book_suggestion.title +
-                            "</td><td>" + book_suggestion.author +
-                            "</td><td>" + book_suggestion.link +
-                            "</td><td>" + book_suggestion.year +
-                            "</td><td>" + book_suggestion.publisher +
-                            "</td><td>" + book_suggestion.editorial +
-                            "</td><td>" + book_suggestion.price +"</td></tr>");
-      },
-      error : function() {
-          var id =$('form').attr('id');
-          console.log("story", id, "story");
-      }
-    });
+    $(document).bind("ajax:success",function(){
+      $('table').append("<tr><td>" + $("input[name='book_suggestion[title]'").val() +
+                        "</td><td>" + $("input[name='book_suggestion[author]'").val() +
+                        "</td><td>" + $("input[name='book_suggestion[link]'").val() +
+                        "</td><td>" + $("input[name='book_suggestion[year]'").val() +
+                        "</td><td>" + $("input[name='book_suggestion[publisher]'").val() +
+                        "</td><td>" + $("input[name='book_suggestion[editorial]'").val() +
+                        "</td><td>" + $("input[name='book_suggestion[price]'").val() +"</td></tr>");
   });
 });
