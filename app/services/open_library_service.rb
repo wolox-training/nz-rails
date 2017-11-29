10060c76ae33c9d52d8732e187cedeb7c2b862b9
nicozar95 <nicolas.zarewsky@wolox.com.ar>
2017-11-29 11:17:11 -0300

    Added strong parameters

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -1,11 +1,11 @@
 class OpenLibraryService
   include HTTParty
 
-  def book_info(isbn)
+  def book_info(isbn_params)
     response = HTTParty.get(
-      "https://openlibrary.org/api/books?bibkeys=ISBN:#{isbn}&format=json&jscmd=data"
+      "https://openlibrary.org/api/books?bibkeys=ISBN:#{isbn_params}&format=json&jscmd=data"
     )
-    parser_json_isbn(response.parsed_response, isbn)
+    parser_json_isbn(response.parsed_response, isbn_params)
   end
 
   private def parser_json_isbn(response, isbn)
@@ -16,4 +16,9 @@ class OpenLibraryService
       number_of_pages: key['number_of_pages'],
       authors: key['authors'] }
   end
+
+  private def isbn_params
+      params.permit(:isbn)
+  end
+
 end
